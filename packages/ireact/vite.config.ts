e45ce7f4a0b47497c421d4c79e5441df331c4368
LuoYanghao <luoyanghao@bytedance.com>
2024-11-08 19:52:18 +0800

    build: compile options

--- packages/ireact/vite.config.ts
+++ packages/ireact/vite.config.ts
@@ -1,11 +1,31 @@
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
     lib: {
-      entry: './lib/main.ts',
-      name: 'Counter',
-      fileName: 'counter',
+      entry: './lib/index.ts',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      output: [
+        {
+          format: 'es',
+          dir: './dist/esm',
+          entryFileNames: '[name].js',
+        },
+        {
+          format: 'cjs',
+          dir: './dist/cjs',
+          entryFileNames: '[name].js',
+        },
+      ],
     },
   },
+  plugins: [
+    dts({
+      outDir: './dist/types',
+      include: ['lib/**/*'],
+    }),
+  ],
 })
